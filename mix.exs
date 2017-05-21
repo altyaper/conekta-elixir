01e9f30e4c36337bcdfce80c3d4b6042ee690190
Jorge Chavez <echavez@nearsoft.com>
2017-05-21 03:18:46 +0000

    Version 1.0.5

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Conekta.Mixfile do
 
   def project do
     [app: :conekta,
-     version: "1.0.4",
+     version: "1.0.5",
      description: @description,
      name: "Conekta",
      elixir: "~> 1.4.2",
