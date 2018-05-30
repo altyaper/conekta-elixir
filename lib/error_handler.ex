0c943739d0e60575045b6b9a71cffd26e73f4346
Ricardo Echavarria <rgechavarria@gmail.com>
2018-05-30 03:28:23 +0000

    Use message because it changes language

--- lib/error_handler.ex
+++ lib/error_handler.ex
@@ -9,7 +9,7 @@ defmodule Conekta.ErrorHandler do
 
     def catch_error(content) do
         {:ok, error} = Poison.decode(content.body, as: %ErrorResponse{})
-        raise ConektaError, message: hd(error.details)["debug_message"]
+        raise ConektaError, message: hd(error.details)["message"]
     end
 
     def raise_error(content) when is_map(content) do
