b7f9c7211dea20c5c3c7e1635a01d73a7e0e1fe7
echavezNS <echavez@nearsoft.com>
2018-10-01 01:10:02 -0700

    Upgrade version of ex_docs

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Conekta.Mixfile do
      {:httpoison, "~> 1.2.0"},
      {:poison, "~> 3.0"},
      {:mock, "~> 0.3.0", only: :test},
-     {:ex_doc, ">= 0.0.0", only: :dev}]
+     {:ex_doc, "~> 0.19", only: :dev, runtime: false}]
   end
 
   defp package do
