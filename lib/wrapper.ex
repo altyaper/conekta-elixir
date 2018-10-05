bb3f44fea337ad69eded5021d31ac22638d2c14e
Kareem Daker <kdaker@gmail.com>
2018-10-05 07:13:09 -0700

    adds version number to config and dynamic accept-header (#8)

--- lib/wrapper.ex
+++ lib/wrapper.ex
@@ -1,7 +1,7 @@
 defmodule Conekta.Wrapper do
     @moduledoc false
     use HTTPoison.Base
-    @conekta_accept_header "application/vnd.conekta-v2.0.0+json"
+    @conekta_accept_header "application/vnd.conekta-v{{version}}+json"
 
     def process_url(url) do
         "https://api.conekta.io/" <> url
@@ -13,7 +13,11 @@ defmodule Conekta.Wrapper do
 
     def headers do
         basic_auth = "Basic " <> Base.encode64(key() <> ":")
-        ["Accept": @conekta_accept_header, "Accept-Language": locale(),"Content-type": "application/json", "Authorization": basic_auth]
+        ["Accept": accept_header(), "Accept-Language": locale(),"Content-type": "application/json", "Authorization": basic_auth]
+    end
+
+    def accept_header do
+        String.replace(@conekta_accept_header, "{{version}}", api_version())
     end
 
     def key do
@@ -24,4 +28,7 @@ defmodule Conekta.Wrapper do
         Application.get_env(:conekta, :locale)
     end
 
+    def api_version do
+        Application.get_env(:conekta, :apiversion)
+    end
 end
