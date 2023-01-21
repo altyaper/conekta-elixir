48e6a564175a790a3b0cc4276550744c0874f7c6
Roberto Estrada <restrada@paridin.com>
2023-01-20 22:50:50 -0600

    Exclusive config per process via process dict.

--- lib/wrapper.ex
+++ lib/wrapper.ex
@@ -3,6 +3,19 @@ defmodule Conekta.Wrapper do
     use HTTPoison.Base
     @conekta_accept_header "application/vnd.conekta-v{{version}}+json"
 
+    @config {__MODULE__, :config}
+
+    def put_config(config) when is_map(config)
+        and is_map_key(config, :privatekey)
+        and is_map_key(config, :locale)
+        and is_map_key(config, :apiversion) do
+        Process.put(@config, config)
+    end
+
+    def get_config(key) do
+        Process.get(@config)[key]
+    end
+
     def process_url(url) do
         "https://api.conekta.io/" <> url
     end
@@ -21,14 +34,17 @@ defmodule Conekta.Wrapper do
     end
 
     def key do
+        get_config(:privatekey) ||
         Application.get_env(:conekta, :privatekey)
     end
 
     def locale do
+        get_config(:locale) ||
         Application.get_env(:conekta, :locale)
     end
 
     def api_version do
+        get_config(:apiversion) ||
         Application.get_env(:conekta, :apiversion)
     end
 end
